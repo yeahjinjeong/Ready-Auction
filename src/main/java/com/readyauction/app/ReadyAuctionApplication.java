6f25fdc18f211f22dc368e5a0d01e9807f90f3a7
jyna120 <jyna120@naver.com>
2024-07-24 21:28:26 +0900

    Feat: Dockerfile 추가

---
+++ src/main/java/com/readyauction/app/ReadyAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.readyauction.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadyAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadyAuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/readyauction/app/ReadyAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readyauction.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadyAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
