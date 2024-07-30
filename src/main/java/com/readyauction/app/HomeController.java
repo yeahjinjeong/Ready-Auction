665088448bfb76a1882f3cad4e0199cf489e6d90
ooANAoo <rkawkaos77@naver.com>
2024-07-30 14:30:53 +0900

    #17 레이아웃 적용완료

--- src/main/java/com/readyauction/app/HomeController.java
+++ src/main/java/com/readyauction/app/HomeController.java
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class HomeController {
-    @Value("${profile.value}")
-    private String value;
+//    @Value("${profile.value}")
+//    private String value;
 
-    @GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
-    public ResponseEntity<?> home(){
-        return ResponseEntity.ok("<h1>⚽️Hello world! App's profile is <mark>%s</mark> \uD83C\uDFB1</h1>".formatted(value));
-    }
+//    @GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
+//    public ResponseEntity<?> home(){
+//        return ResponseEntity.ok("<h1>⚽️Hello world! App's profile is <mark>%s</mark> \uD83C\uDFB1</h1>".formatted(value));
+//    }
 }
