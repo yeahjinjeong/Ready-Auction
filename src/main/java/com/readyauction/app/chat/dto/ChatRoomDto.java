abd0e8f1c69b2554762d2540e9687732b56b07e1
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-08-05 11:07:36 +0900

    Feat: 채팅 회원ID로 참여하고 있는 채팅방 목록 조회 기능 구현 #24

---
+++ src/main/java/com/readyauction/app/chat/dto/ChatRoomDto.java
@@ -0,0 +1,29 @@
+package com.readyauction.app.chat.dto;
+
+import com.readyauction.app.chat.entity.ChatRoom;
+import com.readyauction.app.chat.entity.ChatRoomMember;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomDto {
+    private Long id;
+    private Long productId;
+    private List<ChatRoomMember> chatRoomMembers;
+    private LocalDateTime createdAt;
+
+    public static ChatRoomDto toChatRoomDto(ChatRoom chatRoom) {
+        return new ChatRoomDto(
+                chatRoom.getId(),
+                chatRoom.getProductId(),
+                chatRoom.getChatRoomMembers(),
+                chatRoom.getCreatedAt()
+        );
+    }
+}
---
+++ src/main/java/com/readyauction/app/chat/repository/ChatMessageRepository.java
@@ -0,0 +1,10 @@
+package com.readyauction.app.chat.repository;
+
+import com.readyauction.app.chat.entity.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+//    List<ChatMessage> findChatMessagesByChatRoomId(Long id);
+}
