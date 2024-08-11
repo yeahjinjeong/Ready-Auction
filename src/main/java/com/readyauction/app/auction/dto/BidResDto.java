c80e9c1650f472b4e29d803f9445a102b07f10fd
ooANAoo <rkawkaos77@naver.com>
2024-08-12 03:03:19 +0900

    #40 #41 결제 완료, 입찰, 낙찰 완료

---
+++ src/main/java/com/readyauction/app/auction/dto/BidResDto.java
@@ -0,0 +1,18 @@
+package com.readyauction.app.auction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BidResDto {
+    BidStatus bidStatus;
+    Integer bidCurrentPrice;
+    Timestamp bidSuccessTime;
+}
---
+++ src/main/java/com/readyauction/app/auction/dto/BidStatus.java
@@ -0,0 +1,5 @@
+package com.readyauction.app.auction.dto;
+
+public enum BidStatus {
+    BID, SUCCESS
+}
