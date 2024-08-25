9ec120cf98abebffcb92c0d83a37b25c77f0cd7b
ooANAoo <rkawkaos77@naver.com>
2024-08-26 02:26:09 +0900

    #223

--- src/main/java/com/readyauction/app/auction/RedissonConfig.java
+++ src/main/java/com/readyauction/app/auction/RedissonConfig.java
@@ -7,11 +7,11 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
@@ -42,28 +42,34 @@ public class RedissonConfig {
         config.useSingleServer()
                 .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort)
                 .setPassword(redisPassword)
-                .setConnectionPoolSize(5)  // 최대 연결 수를 6으로 설정 (클라이언트당)
-                .setConnectionMinimumIdleSize(2)  // 최소 유휴 연결 수를 2로 설정
-                .setSubscriptionConnectionPoolSize(3)  // 구독 연결 풀 크기 설정
-                .setSubscriptionConnectionMinimumIdleSize(1)  // 구독 연결 최소 유휴 연결 수 설정
-                .setIdleConnectionTimeout(10000)  // 유휴 연결 타임아웃 설정 (10초)
-                .setConnectTimeout(10000)  // 연결 타임아웃 설정 (10초)
-                .setTimeout(3000)  // 명령어 타임아웃 설정 (3초)
-                .setRetryAttempts(2)  // 재시도 횟수를 2회로 설정
-                .setRetryInterval(1500)  // 재시도 간격 설정 (1.5초)
-                .setPingConnectionInterval(2000)  // 연결 상태 확인을 위한 Ping 간격 설정 (2초)
-                .setClientName("ready-auction-client");  // 클라이언트 이름 설정 (옵션)
+                .setConnectionPoolSize(4)
+                .setConnectionMinimumIdleSize(2)
+                .setSubscriptionConnectionPoolSize(3)
+                .setSubscriptionConnectionMinimumIdleSize(1)
+                .setIdleConnectionTimeout(10000)
+                .setConnectTimeout(10000)
+                .setTimeout(3000)
+                .setRetryAttempts(2)
+                .setRetryInterval(1500)
+                .setPingConnectionInterval(2000)
+                .setClientName("ready-auction-client");
 
         return Redisson.create(config);
     }
 
-    private final RedisProperties redisProperties;
     private final String PATTERN = "__keyevent@*__:expired";
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        // RedisStandaloneConfiguration을 사용하여 비밀번호 설정 추가
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisHost);
+        redisConfig.setPort(redisPort);
+        redisConfig.setPassword(RedisPassword.of(redisPassword));
+
+        return new LettuceConnectionFactory(redisConfig);
     }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
@@ -82,4 +88,3 @@ public class RedissonConfig {
         return redisMessageListenerContainer;
     }
 }
-
