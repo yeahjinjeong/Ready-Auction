a1479080693fabf67b74cc7d3d8e57d3690e9a05
seongyeongit <162237255+seongyeongit@users.noreply.github.com>
2024-08-26 21:14:07 +0900

    Feat: [채팅-신고] 회원은 다른 회원과의 채팅에서 신고를 할 수 있습니다. #259

--- src/main/java/com/readyauction/app/report/service/ReportService.java
+++ src/main/java/com/readyauction/app/report/service/ReportService.java
@@ -1,7 +1,14 @@
 package com.readyauction.app.report.service;
 
+//import com.readyauction.app.report.dto.ChatReportDto;
+import com.readyauction.app.report.dto.ChatReportDto;
 import com.readyauction.app.report.dto.MannerReportDto;
+//import com.readyauction.app.report.entity.ChatReport;
+import com.readyauction.app.report.entity.ChatReport;
+import com.readyauction.app.report.entity.ChatReportStatus;
 import com.readyauction.app.report.entity.MannerReport;
+//import com.readyauction.app.report.repository.ChatReportRepository;
+import com.readyauction.app.report.repository.ChatReportRepository;
 import com.readyauction.app.report.repository.ReportRepository;
 import com.readyauction.app.user.entity.Member;
 import com.readyauction.app.user.repository.MemberRepository;
@@ -9,12 +16,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ReportService {
     public final ReportRepository reportRepository;
     public final MemberRepository memberRepository;
+    private final ChatReportRepository chatReportRepository;
+
+
     public void applyMannerScore(Long id, MannerReportDto mannerReportDto) {
         MannerReport mannerReport = mannerReportDto.toMannerReportEntity(id);
         reportRepository.save(mannerReport);
@@ -25,4 +37,33 @@ public class ReportService {
             member.decreaseMannerScore();
         }
     }
+
+    public void reportChat(ChatReportDto chatReportDto) {
+        ChatReport chatReport = ChatReport.builder()
+                .reporterId(chatReportDto.getReporterId())
+                .reportedMemberId(chatReportDto.getReportedMemberId())
+                .reason(chatReportDto.getReason())
+                .productId(chatReportDto.getProductId())
+                .createdAt(LocalDateTime.now())
+                .detail(chatReportDto.getDetail())
+                .status(ChatReportStatus.PENDING)
+                .build();
+
+        chatReportRepository.save(chatReport);
+    }
+
 }
+
+//    private final ChatReportRepository chatReportRepository;
+
+//    public void saveReport(ChatReportDto reportDto) {
+//        ChatReport chatReport = ChatReport.builder()
+//                .reporterId(reportDto.getReporterId())
+//                .reportedMemberId(reportDto.getReportedMemberId())
+//                .reason(reportDto.getReason())
+//                .chatRoomId(reportDto.getChatRoomId())
+//                .build();
+//
+//        chatReportRepository.save(chatReport);
+//    }
+
